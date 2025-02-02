d021c2bc7c9b07dda6e0fedecf4173f8e40b273f
Dang Trung Hieu <hieudangtrung@protonmail.com>
2025-02-02 16:17:25 +0800

    use alternative API

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    // uri: "http://127.0.0.1:8000/graphql",
-    uri: "https://squerrydb-backend-production.up.railway.app/graphql",
+    uri: "https://squerrydb-backend-production-db4d.up.railway.app/graphql",
     cache: new InMemoryCache(),
   });
 
