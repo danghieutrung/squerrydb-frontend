2fe4c31167229a476be7e1fd5e80ae2207c518bc
Dang Trung Hieu <hieudangtrung@protonmail.com>
2025-02-03 14:48:03 +0800

    add demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,30 @@ import "../app/globals.css";
 
 export default function Home() {
   return (
-    <div>
-      <NavBar displayNavSearchBar={false} />
-      <div className="p-20">
+    <div className="min-h-screen flex flex-col">
+      <NavBar displayNavSearchBar={true} />
+      <div className="flex flex-col items-center w-full pt-[15vh] p-10">
         <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-center">
           IMDb Series Search
         </h1>
-        <SearchBar />
+
+        <div className="flex flex-col items-center w-full">
+          <div className="w-full max-w-md">
+            <SearchBar />
+          </div>
+
+          <img
+            src="/demo.gif"
+            alt="IMDb Search Demo"
+            className="pt-[5vh] w-full max-w-md rounded-lg shadow-lg"
+          />
+        </div>
+      </div>
+      <div className="fixed bottom-4 w-full text-center text-sm text-gray-600">
+        Contact me at{" "}
+        <a href="mailto:lol@lol.com" className="underline">
+          hieudangtrung@protonmail.com
+        </a>
       </div>
     </div>
   );
