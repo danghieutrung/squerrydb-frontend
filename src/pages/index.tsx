22a9b5fad8c74d141d0bd09e41740443344d76ad
Dang Trung Hieu <hieudangtrung@protonmail.com>
2025-02-03 10:39:39 +0800

    reponsive NavBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import "../app/globals.css";
 export default function Home() {
   return (
     <div>
-      <NavBar displayNavSearchBar={false} />
+      <NavBar displayNavSearchBar={true} />
       <div className="p-20">
         <h1 className="text-3xl font-bold mb-4 text-center">
           IMDb Series Search
